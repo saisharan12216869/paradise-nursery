2f945e1b8d5803bf9b8b929d5c594dcd6243ffa9
sai sharan <119558786+saisharan12216869@users.noreply.github.com>
2025-12-28 20:07:38 +0530

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
-import { increaseQty, decreaseQty, removeItem } from "../redux/CartSlice";
+import { updateQuantity, removeItem } from "../redux/CartSlice";
+
 import Navbar from "./Navbar";
 
 export default function CartItem() {
